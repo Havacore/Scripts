f16652d399a0d40eaf649da315ce4c3d39b2abf7
jdahl-va <jdahl@vendasta.com>
2015-12-02 09:28:01 -0600

    advent of code

--- dockerupdate.py
+++ dockerupdate.py
@@ -12,14 +12,19 @@ def main():
 
 	containerId = containerId[:12]
 
-	Popen(["docker", "build", "-t", "ws-proxy", "."])
-	Popen(["docker", "kill", containerId], stdout=PIPE)
-	p2 = Popen(["docker", "run", "-d", "-p", "80:80", "ws-proxy"], stdout=PIPE)
+	poll(Popen(["docker", "build", "-t", "ws-proxy", "."]))
+	poll(Popen(["docker", "kill", containerId], stdout=PIPE))
+	p2 = poll(Popen(["docker", "run", "-d", "-p", "80:80", "ws-proxy"], stdout=PIPE))
 	for line in p2.stdout:
 		new_process = line
 	print new_process
 
-	Popen(["docker", "logs", "-f", new_process], stdout=PIPE)
+	Popen(["docker", "logs", "-f", new_process])
+
+def poll(process):
+	while process.poll() is None:
+    l = process.stdout.readline() # This blocks until it receives a newline.
+    print l
 
 if __name__ == "__main__":
     main()
